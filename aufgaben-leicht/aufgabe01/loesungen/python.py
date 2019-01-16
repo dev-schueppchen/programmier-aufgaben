9474eb3f140c568b7be63bed309c685f11494bff
Ron31 <ron3108@gmx.net>
2019-01-16 19:24:57 +0100

    Create python.py

---
+++ aufgaben-leicht/aufgabe01/loesungen/python.py
@@ -0,0 +1,11 @@
+userpws = {"root": "rootpw"}
+
+print('User:')
+user = input()
+print('Password:')
+password = input()
+
+if(password == userpws.get(user)):
+    print('Du bist nun eingeloggt')
+else:
+    print('Falsches Passwort')
