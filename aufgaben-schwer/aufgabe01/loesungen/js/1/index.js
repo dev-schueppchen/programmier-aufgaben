eabf893b3c9e1a5395460631860ce3ee556742f6
swarfey <45124381+swarfey@users.noreply.github.com>
2020-06-06 18:03:23 +0200

    Add files via upload

---
+++ aufgaben-schwer/aufgabe01/loesungen/js/1/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const WebSocket = require('ws');
+const app = express();
+// const server = require('http').createServer(app)
+const wss = new WebSocket.Server({ port: 1337 });
+
+
+
+
+wss.on('connection', ws => {
+    ws.on('message', (data) => {
+        console.log(data)
+        try {
+            var json = JSON.parse(data)
+            if (json.username == "" || json.content == "") return ws.send("")
+            wss.clients.forEach(_ws => {
+                var time = new Date()
+                _ws.send(`${json.username} @ ${time}: ${json.content}`)
+            }) 
+        } catch (e) {
+            ws.send("wrong json/no json")
+        }
+    })
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'\\chat.html')
+})
+
+app.listen(8080)
